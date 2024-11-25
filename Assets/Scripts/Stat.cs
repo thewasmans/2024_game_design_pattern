9a7e8fc7c2b4ce9681c10ff4a57c2790acef6935
thewasmans <thewasmans@gmail.Com>
2024-11-25 14:44:17 +0100

    steve local work

--- Assets/Scripts/Stat.cs
+++ Assets/Scripts/Stat.cs
@@ -1,5 +1,8 @@
 public class Stat
 {
+    /// <summary>
+    /// LOL
+    /// </summary>
     public float Value{get;set;}
     public float ModifiedValue{get;set;}
     public string Type{get;set;}
